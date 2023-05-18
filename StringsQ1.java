e719f6ad26bff21a64deb33609e71d901eb7cff4
Ashutosh Kumar <82165977+ashutoshhh0@users.noreply.github.com>
2023-05-18 22:59:31 +0530

    StringsQ1

---
+++ StringsQ1.java
@@ -0,0 +1,7 @@
+public class StringsQ1 {
+    public static void main(String args[]) {
+        String s1 = "“Hello World";
+        System.out.println("string length is: " + s1.length());
+    }
+
+}
